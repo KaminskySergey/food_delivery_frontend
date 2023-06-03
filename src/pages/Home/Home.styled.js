e3588bcbc1f392a60b260e9689927f825aa30b9b
KaminskySergey <kaminsky_s_s@ukr.net>
2023-06-04 01:02:25 +0300

    styled

--- src/pages/Home/Home.styled.js
+++ src/pages/Home/Home.styled.js
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import { styled } from "styled-components";
+import styled from 'styled-components';
 
 export const Link = styled(NavLink)`
   background-color: #FF5722;
